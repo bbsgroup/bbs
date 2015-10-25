e52e2f5beb8ac8cf6a7b58fa7b1d5498b8f9c632
hezh <952113365@qq.com>
2015-10-25 09:42:57 +0800

    添加用户是否登录的session拦截在，添加异常处理，并跳转到登录页面

---
+++ bbs/src/main/java/org/base/exception/SessionTimeoutException.java
@@ -0,0 +1,10 @@
+package org.base.exception;
+
+public class SessionTimeoutException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3386225402178690871L;
+
+}
---
+++ bbs/src/main/java/org/bbs/interceptor/SessionTimeOutInterceptor.java
@@ -0,0 +1,45 @@
+package org.bbs.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.base.exception.SessionTimeoutException;
+import org.bbs.entity.User;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class SessionTimeOutInterceptor extends HandlerInterceptorAdapter {
+	
+	
+	 public String[] allowUrls;//还没发现可以直接配置不拦截的资源，所以在代码里面来排除  
+     
+	    public void setAllowUrls(String[] allowUrls) {  
+	        this.allowUrls = allowUrls;  
+	    }  
+
+
+	@Override
+	public boolean preHandle(HttpServletRequest request,
+			HttpServletResponse response, Object handler) throws Exception {
+	      String requestUrl = request.getRequestURI().replace(request.getContextPath(), "");    
+	        System.out.println(requestUrl);  
+	        if(null != allowUrls && allowUrls.length>=1)  
+	            for(String url : allowUrls) {    
+	                if(requestUrl.contains(url)) {    
+	                    return true;    
+	                }    
+	            }  
+	          
+	        User user = (User) request.getSession().getAttribute("currentUser");  
+	        if(user != null) {    
+	            return true;  //返回true，则这个方面调用后会接着调用postHandle(),  afterCompletion()  
+	        }else{  
+	            // 未登录  跳转到登录页面  
+	            throw new SessionTimeoutException();//返回到配置文件中定义的路径  
+	        }  
+	    } 
+		
+		
+	}
+	
+
+
